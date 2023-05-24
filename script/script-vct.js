58223317232f81d3a776e6144bd3fd81e6523ca6
CruelGs <gustavopamponet963@gmail.com>
2023-05-23 22:23:30 -0300

    up

--- script/script-vct.js
+++ script/script-vct.js
@@ -4,4 +4,13 @@ function c(){
         {itens.style.display = 'none'}
     else 
         {itens.style.display = 'block'}
+
+    itens.style.display = block
+}
+
+function tamanho(){
+    if (window.innerWidth >= 670)
+        {itens.style.display = 'block'}
+    else
+        {itens.style.display = 'none'}
 }
